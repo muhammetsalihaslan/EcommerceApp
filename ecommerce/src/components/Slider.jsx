75376691806c5d5adae05236bdbab08dc4be236d
salih <muhammetsalihaslan@gmail.com>
2022-11-09 23:38:45 +0300

    slide

--- ecommerce/src/components/Slider.jsx
+++ ecommerce/src/components/Slider.jsx
@@ -40,7 +40,8 @@ const Arrow = styled.div`
 const Wrapper = styled.div`
    height: 100%;
    display: flex;
-   transform: translateX(0vw);
+   transform: translateX(${props=>props.slideIndex* -100}vw);
+   transition: all 1.5s ease
 
 `
 
@@ -99,14 +100,19 @@ const Slider = () => {
     const [slideIndex , setSlideIndex] = useState(0)
 
     const handleClick = (direction) => {
-
+      
+      if(direction==="left"){
+        setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2 )
+      } else {
+        setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0 )
+      }
     };
   return (
     <Container>
         <Arrow direction="left" onClick={()=>handleClick("left")}>
         <ArrowBackIosNewOutlinedIcon/>
         </Arrow>
-        <Wrapper>
+        <Wrapper slideIndex={slideIndex}>
          {sliderItems.map((item) =>(
 
 
