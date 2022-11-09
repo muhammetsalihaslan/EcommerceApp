b41dc76fc65d00866b58bb850f07dd359279d48b
salih <muhammetsalihaslan@gmail.com>
2022-11-09 14:35:17 +0300

    navbar done

--- ecommerce/src/components/Navbar.jsx
+++ ecommerce/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import SearchIcon from '@mui/icons-material/Search';
+import Badge from '@mui/material/Badge';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const Container = styled.div`
    height:60px;
@@ -34,16 +36,32 @@ const SearchContainer = styled.div`
 `
 
 const Input = styled.input`
-   
+   border:none;
 `
 
 
 
 const Center = styled.div`
-  flex: 1;`;
+  flex: 1;
+  text-align: center;
+  `;
+
+const Logo = styled.h1`
+   font-weight: bold;
+`
 
 const Right = styled.div`
-  flex: 1;`;
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content:flex-end;
+  `;
+
+const MenuItem = styled.div`
+     font-size: 14px;
+     cursor: pointer;
+     margin-left: 25px;
+`
 
 
 
@@ -58,11 +76,24 @@ const Navbar = () => {
             </Language>
             <SearchContainer>
                 <Input/>
-                <SearchIcon/>
+                <SearchIcon style={{color:"gray", fontSize:16}}/>
             </SearchContainer>
         </Left>
-        <Center>Center</Center>
-        <Right>Right</Right>
+        <Center>
+            <Logo>
+                BrandWay
+            </Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+              <Badge badgeContent={4} color="primary">
+                <ShoppingCartOutlinedIcon/>
+              </Badge>
+          </MenuItem>
+
+        </Right>
 
         </Wrapper>
     </Container>
