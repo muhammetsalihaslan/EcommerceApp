00ddab2c6a4e75b2aa4c37ab853d6b4fcaab1810
salih <muhammetsalihaslan@gmail.com>
2022-11-09 14:15:09 +0300

    navbar

--- ecommerce/src/components/Navbar.jsx
+++ ecommerce/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import SearchIcon from '@mui/icons-material/Search';
 
 const Container = styled.div`
    height:60px;
@@ -9,17 +10,57 @@ const Container = styled.div`
 const Wrapper = styled.div`
   padding:10px 20px;
   display: flex;
+  justify-content:space-between;
+  align-items:center;
 `
 
-const Left = styled.div``
-const Center = styled.div``
-const Right = styled.div``
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items:center;
+  `;
+
+const Language = styled.span`
+    font-size:14px;
+    cursor: pointer;
+`
+
+const SearchContainer = styled.div`
+   border: 1px solid lightgray;
+   display: flex;
+   align-items:center;
+   margin-left:25px;
+   padding:5px;
+`
+
+const Input = styled.input`
+   
+`
+
+
+
+const Center = styled.div`
+  flex: 1;`;
+
+const Right = styled.div`
+  flex: 1;`;
+
+
+
 
 const Navbar = () => {
   return (
     <Container>
         <Wrapper>
-        <Left>Left</Left>
+        <Left>
+            <Language>
+                EN
+            </Language>
+            <SearchContainer>
+                <Input/>
+                <SearchIcon/>
+            </SearchContainer>
+        </Left>
         <Center>Center</Center>
         <Right>Right</Right>
 
