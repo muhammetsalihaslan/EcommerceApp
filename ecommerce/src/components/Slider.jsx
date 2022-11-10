607d0467adf578ba28976700291e530633dd5047
salih <muhammetsalihaslan@gmail.com>
2022-11-10 12:01:37 +0300

    components

--- ecommerce/src/components/Slider.jsx
+++ ecommerce/src/components/Slider.jsx
@@ -116,10 +116,10 @@ const Slider = () => {
          {sliderItems.map((item) =>(
 
 
-         <Slide bg={item.bg} >
+         <Slide bg={item.bg} key={item.id} >
 
           <ImgContainer>
-            <Image src={item.img}/>
+            <Image src={item.img} />
           </ImgContainer>
            <InfoContainer >
             <Title>{item.title}</Title>
