c282797bd064c121010f40f4000a69ee6de5c1f7
salih <muhammetsalihaslan@gmail.com>
2022-11-11 16:06:47 +0300

    product single page

---
+++ ecommerce/src/pages/Product.jsx
@@ -0,0 +1,117 @@
+import React from 'react'
+import styled from 'styled-components';
+import Navbar from '../components/Navbar';
+import Announcement from '../components/Announcement';
+import Newsletter from '../components/Newsletter';
+import Footer from '../components/Footer';
+
+
+const Container = styled.div``
+
+
+const Wrapper = styled.div`
+   padding: 50px;
+   display: flex;
+`
+
+
+const ImgContainer = styled.div`
+   flex: 1;
+   padding: 0px 50px;
+`
+
+
+const Image = styled.img`
+    width: 100%;
+    height: 90vh;
+    object-fit: cover;
+`
+
+
+const InfoContainer = styled.div`
+   flex: 1;
+`
+
+
+const Title = styled.h1`
+   font-weight: 200;
+
+`
+
+
+const Desc = styled.p`
+   margin: 20px 0px;
+`
+
+
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`
+
+
+const FilterContainer = styled.div``
+    
+
+const Filter = styled.div``
+
+
+const FilterTitle = styled.h1``
+    
+
+const FilterColor = styled.select``
+
+
+
+const FilterSize = styled.select``
+
+
+const FilterSizeOption = styled.option``
+    
+
+
+
+
+
+const Product = () => {
+  return (
+    <Container>
+        <Navbar/>
+        <Announcement/>
+        <Wrapper>
+            <ImgContainer>
+            <Image/>
+            </ImgContainer>
+            <InfoContainer>
+                <Title></Title>
+                <Desc></Desc>
+                <Price></Price>
+            <FilterContainer>
+                <Filter>
+                    <FilterTitle>Color</FilterTitle>
+                    <FilterColor color="black" />
+                    <FilterColor color="darkblue"/>
+                    <FilterColor color="gray"/>
+                </Filter>
+                <Filter>
+                    <FilterTitle>Size</FilterTitle>
+                    <FilterSize>
+                        <FilterSizeOption>s</FilterSizeOption>
+                        <FilterSizeOption>m</FilterSizeOption>
+                        <FilterSizeOption>l</FilterSizeOption>
+                        <FilterSizeOption>xl</FilterSizeOption>
+                        <FilterSizeOption>xxl</FilterSizeOption>
+                    </FilterSize>
+                    
+                </Filter>
+            </FilterContainer>
+            </InfoContainer>
+        </Wrapper>
+        <Newsletter/>
+        <Footer/>
+
+    </Container>
+  )
+}
+
+export default Product
