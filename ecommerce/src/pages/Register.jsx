70763cef4f6438c8b7eca22ca51538627a7d862c
salih <muhammetsalihaslan@gmail.com>
2022-11-12 14:54:12 +0300

    REGİSTER

--- ecommerce/src/pages/Register.jsx
+++ ecommerce/src/pages/Register.jsx
@@ -16,23 +16,47 @@ const Container = styled.div`
 
 
 const Wrapper = styled.div`
-    
+    width: 40%;
+    padding: 20px;
+    background-color:white;
+
 `
 
 
-const Title = styled.h1``
+const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 300;
+`
 
 
-const Form = styled.form``
+const Form = styled.form`
+   display: flex;
+   flex-wrap: wrap;
+`
 
 
-const Input = styled.input``
+const Input = styled.input`
+    flex:1;
+    min-width:40%;
+    margin:20px 10px 0px 0px;
+    padding: 10px;
+`
 
 
-const Agreement = styled.span``
+const Agreement = styled.span`
+   font-size: 12px;
+   margin:20px 0px;
+`
     
 
-const Button = styled.button``
+const Button = styled.button`
+   width: 40%;
+   border:none;
+   padding: 15px 20px;
+   background-color:teal;
+   color:white;
+   cursor: pointer;
+`
 
 
 
@@ -43,7 +67,7 @@ const Register = () => {
   return (
     <Container>
         <Wrapper>
-            <Title>Create AN ACCOUNT</Title>
+            <Title>CREATE AN ACCOUNT</Title>
             <Form>
                 <Input placeholder="name"/>
                 <Input placeholder="last name"/>
