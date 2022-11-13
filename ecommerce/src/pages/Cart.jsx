8344d57ed5c0e2f02106b6b8832286e6fc6c3115
salih <muhammetsalihaslan@gmail.com>
2022-11-13 15:18:23 +0300

    cart

--- ecommerce/src/pages/Cart.jsx
+++ ecommerce/src/pages/Cart.jsx
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import Navbar from '../components/Navbar'
 import Announcement from '../components/Announcement'
 import Footer from '../components/Footer'
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 
 const Container = styled.div``
@@ -58,20 +60,31 @@ const Info = styled.div`
 `
 
 
-const Summary = styled.div`
-   flex:1;
+
+
+const Product = styled.div`
+   display: flex;
+   justify-content: space-between;
 `
 
-const Product = styled.div``
 
-
-const ProductDetail = styled.div``
+const ProductDetail = styled.div`
+   flex:2;
+   display: flex;
+`
     
 
-const Image = styled.img``
+const Image = styled.img`
+   width: 200px;
+`
 
 
-const Details = styled.div``
+const Details = styled.div`
+   padding: 20px;
+   display: flex;
+   flex-direction: column;
+   justify-content: space-around;
+`
 
 
 const ProductName = styled.span``
@@ -80,13 +93,73 @@ const ProductName = styled.span``
 const ProductId = styled.span``
 
 
-const ProductColor = styled.span``
+const ProductColor = styled.div`
+   width: 20px;
+   height: 20px;
+   border-radius: 50%;
+   background-color: ${props => props.color};
+`
 
 
 const ProductSize = styled.span``
 
 
-const PriceDetail = styled.div``
+const PriceDetail = styled.div`
+   flex:1;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+`
+
+const ProductAmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+`
+    
+
+const ProductAmount = styled.div`
+   font-size: 24px;
+   margin: 5px;
+`
+
+
+const ProductPrice = styled.div`
+  font-size: 30px;
+  font-weight: 200;
+`
+
+const Hr = styled.hr`
+  background-color:#eee;
+  border:none;
+  height:1px;
+`
+
+const Summary = styled.div`
+   flex:1;
+   border:0.5px solid lightgray;
+   border-radius:10px;
+   padding:20px;
+   height: 50vh;
+`
+
+const SummaryTitle = styled.h1`
+    font-weight: 200;
+    
+
+`
+
+const SummaryItem = styled.div``
+    
+
+const SummaryItemText = styled.span``
+    
+
+const SummaryItemPrice = styled.span``
+    
+
+const Button = styled.button``
 
 
 
@@ -115,16 +188,60 @@ const Cart = () => {
                        <Details>
                          <ProductName> <b>Product:</b>  Bohemian Beach Dress</ProductName>
                          <ProductId> <b>ID:</b>  80080090060</ProductId>
-                         <ProductColor/>
+                         <ProductColor color="green"/>
                         <ProductSize> <b>Size:</b> 38 </ProductSize>
                        </Details>
                     </ProductDetail>
                     <PriceDetail>
-                       price
+                       <ProductAmountContainer>
+                         <AddIcon/>
+                         <ProductAmount>2</ProductAmount>
+                         <RemoveIcon/>
+                       </ProductAmountContainer>
+                       <ProductPrice>$30</ProductPrice>
+                    </PriceDetail>
+                </Product>
+                <Hr/>
+                <Product>
+                    <ProductDetail>
+                       <Image src="https://cdn.shopify.com/s/files/1/0580/2193/7324/products/product-image-1942292165_1024x.jpg?v=1643831031"/>
+                       <Details>
+                         <ProductName> <b>Product:</b> Tallula Dress</ProductName>
+                         <ProductId> <b>ID:</b>  80080090054</ProductId>
+                         <ProductColor color="black"/>
+                        <ProductSize> <b>Size:</b> 38 </ProductSize>
+                       </Details>
+                    </ProductDetail>
+                    <PriceDetail>
+                       <ProductAmountContainer>
+                         <AddIcon/>
+                         <ProductAmount>2</ProductAmount>
+                         <RemoveIcon/>
+                       </ProductAmountContainer>
+                       <ProductPrice>$35</ProductPrice>
                     </PriceDetail>
                 </Product>
               </Info>  
-              <Summary>summary</Summary>
+              <Summary>
+                <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+                <SummaryItem>
+                    <SummaryItemText>Subtotal</SummaryItemText>
+                    <SummaryItemPrice>$ 70</SummaryItemPrice>
+                </SummaryItem>
+                <SummaryItem>
+                    <SummaryItemText>Estimated Shipping</SummaryItemText>
+                    <SummaryItemPrice>$ 5.90</SummaryItemPrice>
+                </SummaryItem>
+                <SummaryItem>
+                    <SummaryItemText>Shipping Discount</SummaryItemText>
+                    <SummaryItemPrice>$ -5.90</SummaryItemPrice>
+                </SummaryItem>
+                <SummaryItem>
+                    <SummaryItemText type="total">Total</SummaryItemText>
+                    <SummaryItemPrice>$ 70</SummaryItemPrice>
+                </SummaryItem>
+                <Button>CHECK OUT NOW</Button>
+              </Summary>
             </Bottom>
         </Wrapper>
         <Footer/>
