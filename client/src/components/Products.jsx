dfc87c9951131e8e6830aa45e52e48f801e61b30
salih <muhammetsalihaslan@gmail.com>
2022-12-01 15:07:24 +0300

    axios

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import { popularProducts } from '../data'
-import Product from './Product'
+import Product from './Product';
+import { useState } from "react";
+import axios from 'axios';
 
 const Container = styled.div`
    padding: 20px;
@@ -9,7 +11,21 @@ const Container = styled.div`
    flex-wrap: wrap;
 `
 
-const Products = () => {
+const Products = ({cat, filters,sort}) => {
+  
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+     const getProducts = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/api/products");
+        console.log(res)
+      } catch (err) {};
+    }
+    getProducts()
+  }, [cat]);
+
   return (
     <Container>
         {popularProducts.map(item=>(
