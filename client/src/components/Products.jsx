e358aa57d267934756a439aba5c53a51d4ecf45c
salih <muhammetsalihaslan@gmail.com>
2022-12-01 16:54:01 +0300

    products

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,38 +1,54 @@
-import React, { useEffect } from 'react'
-import styled from 'styled-components'
-import { popularProducts } from '../data'
-import Product from './Product';
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { popularProducts } from "../data";
+import Product from "./Product";
 import { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 const Container = styled.div`
-   padding: 20px;
-   display: flex;
-   flex-wrap: wrap;
-`
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+`;
 
-const Products = ({cat, filters,sort}) => {
-  
+const Products = ({ cat, filters, sort }) => {
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(() => {
-     const getProducts = async () => {
+    const getProducts = async () => {
       try {
-        const res = await axios.get("http://localhost:5000/api/products");
-        console.log(res)
-      } catch (err) {};
-    }
-    getProducts()
+        const res = await axios.get(
+          cat
+            ? `http://localhost:5000/api/products=${cat}`
+            : "http://localhost:5000/api/products"
+        );
+        setProducts(res.data);
+      } catch (err) {}
+    };
+    getProducts();
   }, [cat]);
+
+  useEffect(() => {
+    cat &&
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+  }, [products, cat, filters]);
 
   return (
     <Container>
-        {popularProducts.map(item=>(
-        <Product item={item} key={item.id}/>
-        ))}
+      {cat
+        ? filteredProducts.map((item) => <Product item={item} key={item.id} />)
+        : products
+            .slice(0, 8)
+            .map((item) => <Product item={item} key={item.id} />)}
     </Container>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
