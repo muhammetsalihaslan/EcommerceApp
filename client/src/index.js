4158e06cf5f18b805fc3b5e9453fe51dda36c48c
salih <muhammetsalihaslan@gmail.com>
2022-12-03 17:56:44 +0300

    saturday work

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,9 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-
-  document.getElementById("root")
+  </Provider>
 );
