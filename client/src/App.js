f94dc100c4aae021b2ae5d3c76974b60447f767e
salih <muhammetsalihaslan@gmail.com>
2022-12-07 10:42:02 +0300

    login page

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import Success from "./pages/Success";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  const user = true;
+  const user = false;
 
   return (
     <BrowserRouter>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import styled from "styled-components";
 import { mobile } from "../responsive";
 import { useState } from "react";
+import { login } from "../redux/apiCalls";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 const Container = styled.div`
   width: 100vw;
@@ -51,6 +54,10 @@ const Button = styled.button`
   color: white;
   cursor: pointer;
   margin-bottom: 5px;
+  &:disabled {
+    color: green;
+    cursor: not-allowed;
+  }
 `;
 
 const Link = styled.a`
@@ -60,19 +67,40 @@ const Link = styled.a`
   cursor: pointer;
 `;
 
+const Error = styled.span`
+  color: red;
+`;
+
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const { isFetching, error } = useSelector((state) => state.user);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    login(dispatch, { username, password });
+  };
   return (
     <Container>
       <Wrapper>
         <Title>SIGN IN</Title>
         <Form>
-          <Input placeholder="email" />
+          <Input
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
 
-          <Input placeholder="password" />
+          <Input
+            placeholder="password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <Button>LOGIN</Button>
+          <Button onClick={handleClick} disabled={isFetching}>
+            LOGIN
+          </Button>
+          {error && <Error>Something went wrong...</Error>}
 
           <Link>Forget Password</Link>
           <Link>Create a new account</Link>
